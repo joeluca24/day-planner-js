87051f0b54151739700f45b55794928a53b22043
joeluca24 <65137591+joeluca24@users.noreply.github.com>
2020-07-26 22:14:40 -0400

    add working main js

--- main.js
+++ main.js
@@ -1,2 +1,49 @@
 moment().format('MMMM Dao YYYY, h:mm:ss ');
 moment().subtract(10, 'days').calendar();
+
+
+$("button").on('click', function(event){
+    event.preventDefault();
+    console.log(this);
+    var parent = $(this).parent();
+    var id = parent.attr('id');
+    var value = parent.find("textarea").val()
+    console.log({id, value})
+
+    localStorage.setItem(id, value);
+
+    //display has to run to load saved values
+});
+
+// display function definition
+function display(){
+    console.log("display");
+    $("textarea").each(function(){
+        console.log(this);
+        var parent = $(this).parent();
+        var id = parent.attr('id');
+        console.log(id);
+        var value = localStorage.getItem(id)
+        console.log(value)
+        if(value === null){
+            console.log("no value yet");
+        }
+        else{
+            $(this).val(value)
+        }
+    });
+}
+    
+// display has to run on page load
+display();
+
+// $(this).parent().get(0)
+// <div class=​"row" id=​"time-9">​…​</div>​
+// $(this).parent().find('textarea').get(0)
+// <textarea class=​"col-md-10 textarea">​lunch at 2​</textarea>​
+// $(this).parent().find('textarea').val()
+// "lunch at 2"
+// $(this).parent().attr('id');
+// "time-9"
+
+//localStorage.setItem("time-9", "lunch at 2")
